13827699b0d42c850780a5489bfa5509c374005b
Anish Singh Chandel <123861376+ChandelAnish@users.noreply.github.com>
2024-10-16 23:27:39 +0530

    minor changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   // uri: 'http://localhost:4000/graphql',//graphQL endpoint i.e our server url
-  uri: 'https://graphql-l8ui.onrender.com',//graphQL endpoint i.e our server url
+  uri: 'https://graphql-l8ui.onrender.com/graphql',//graphQL endpoint i.e our server url
   cache: new InMemoryCache(),// Apollo Client uses to cache query results after fetching them.
   credentials: "include"// This tells Apollo Client to send cookies along with every request to the server.
 });
